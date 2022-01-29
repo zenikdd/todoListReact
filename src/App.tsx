670884e81a1373c587ea44cf2cdd8df993b51e3f
eugene.zenkov <eugene.zenkou@gmail.com>
2022-01-29 08:43:32 +0300

    Preparation to lesson 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react"
 import './App.scss';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, useHistory} from "react-router-dom";
 import AddTodos from "./component/add-todos.component";
 import LoginPage from "./component/login.page";
 import RegisterPage from "./component/register.page";
@@ -14,7 +14,7 @@ const App = () => {
             <Route exact path='/todos'>
                 <AddTodos/>
             </Route>
-            <Route exact path='/login'>
+            <Route path='/'>
                 <LoginPage></LoginPage>
             </Route>
             <Route exact path='/register'>
--- src/component/register.page.tsx
+++ src/component/register.page.tsx
@@ -3,14 +3,17 @@ import TextField from "@mui/material/TextField";
 import './register.page.scss';
 import {useForm} from "react-hook-form";
 import {registerUser} from "../common/api/user.api";
+import {useHistory} from "react-router-dom";
 
 
 const RegisterPage = () => {
     const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const history = useHistory();
 
     const onSubmit = async (data: any) => {
-        const res: any = await registerUser(data);
-        localStorage.setItem('token', res.token)
+        // const res: any = await registerUser(data);
+        // localStorage.setItem('token', res.token)
+        history.push('/todos')
     };
 
     return (
